0e5002a8f2e241f52a8d204da3227cb8555fdd97
Sithum Witharana <witharana92@gmail.com>
2024-01-03 10:46:32 +0530

    Two Way Bind

--- Tangy_Web_Server/Pages/Error.cshtml.cs
+++ Tangy_Web_Server/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace TangyWeb_Server.Pages
+namespace Tangy_Web_Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
--- Tangy_Web_Server/Program.cs
+++ Tangy_Web_Server/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,10 +17,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-else
-{
-    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
